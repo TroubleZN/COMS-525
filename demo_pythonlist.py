3ddd35323272d4e8e07038dde1c0eb92e3fad64f
Ni <zhengni@h4tj1149q6x3.math.iastate.edu>
2023-01-24 12:37:27 -0600

    test

---
+++ demo_pythonlist.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = []
+
+a.append(1)
+print(a)
+a.append("asd")
+print(a)
+
+a.clear()
+print(a)
+
+
+# append(), clear(), copy(), count(), extend(), index(),
+# insert(), pop(), remove(), reverse(), sort().
+
