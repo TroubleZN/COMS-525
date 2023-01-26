e6b5eedd2667044ec05198f00368fd311016289f
Ni <zhengni@h4tj1149q6x3.math.iastate.edu>
2023-01-26 11:59:57 -0600

    My factorial function added

--- lab4/myfuncs.py
+++ lab4/myfuncs.py
@@ -1,10 +1,15 @@
 """
-My factorial function
+My sqrt function
 """
 
 def factorial(n):
+    """
+    My factorial function
+    """
     s = 1
     for i in range(1,n):
         s = s * (i + 1)
     return s
 
+
+def 
