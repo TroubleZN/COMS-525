b9d07de94d46e154d0f34bfc11f66694508efdfd
Ni <zhengni@h4tj1149q6x3.math.iastate.edu>
2023-01-31 12:02:28 -0600

    The header of the solver function is added

---
+++ lab5/gauss_elimination_sovle.py
@@ -0,0 +1,18 @@
+"""
+My Gauss elimination solver
+"""
+
+def mysolver(A,b):
+    """
+    This is a linear solver based on Gauss elimination
+
+    Model: 
+        Ax = b
+    Inupt:
+        A: Float matrix (dims n x n)
+        b: Float matrix (dims n x 1)
+    Output:
+        x: Float matrix (dims n x 1)
+    """
+
+    
