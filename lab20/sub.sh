ce0464bfb12be1c255d32d45159ef3fcb1f9570b
TroubleZN <zhengni97@gmail.com>
2023-03-31 18:00:56 -0500

    lab20

---
+++ lab20/sub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Copy/paste this job script into a text file and submit with the command:
+#    sbatch thefilename
+# job standard output will go to the file slurm-%j.out (where %j is the job ID)
+
+#SBATCH --time=00:15:00   # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=36   # 36 processor core(s) per node 
+#SBATCH --mem=4G   # maximum memory per node
+#SBATCH --job-name="CS525"
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
+./main.exe 3 1.0e-6
